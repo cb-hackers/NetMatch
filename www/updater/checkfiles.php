8a3d19db6c1977b44d0aaaaa79e5d59d234aa779
VesQ <laakso.vesa@gmail.com>
2011-03-03 18:16:08 +0200

    Updateria varten CRC32-tarkistus PHP-puolella

---
+++ www/updater/checkfiles.php
@@ -0,0 +1,29 @@
+<?
+header( "Content-type: text\plain;" );
+define( "SCRIPT_DIR", "http://netmatch.vesq.org/updater/" );
+
+gohash();
+
+function gohash( $dir = "files/" )
+{
+
+    $fileArray = scandir( $dir );
+    foreach( $fileArray as $file )
+    {
+        if( $file != "." && $file != ".." )
+        {
+            if( $dir != "." )
+                $file = $dir . "/" . $file;
+            if( is_dir( $file ) )
+            {
+                gohash( $file );
+                continue;
+            }
+            echo $file . "|";
+            echo strtoupper(hash_file('crc32b', $file)) . "|";
+            echo SCRIPT_DIR;
+            echo "$file\n";
+        }
+    }
+}
+?>
